431e8f1fcca7b4413da251852ffccde3786a7795
shinjjune <sjh8236@naver.com>
2019-11-12 15:06:01 +0900

    login

--- routers/user.js
+++ routers/user.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const { User, validateUser } = require("../models/user");
+const { jwtSecret } = require("../common/jwt_config");
 
+// 회원가입
 router.post("/join", async (req, res, next) => {
   const { name, email, password } = req.body;
   if (validateUser(req.body).error) {
@@ -19,4 +21,35 @@ router.post("/join", async (req, res, next) => {
   res.json({ result: true });
   next();
 });
+
+// 로그인
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    res.json({ result: false });
+    next();
+    return;
+  }
+  const result = await bcrypt.compare(password, user.password);
+  if (result) {
+    // 토큰을 만들어 줍시다!
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin
+      },
+      jwtSecret,
+      { expiresIn: "1m" }
+    );
+    res.json({ result: true, token, admin: user.admin });
+    next();
+  } else {
+    res.json({ result: false });
+    next();
+  }
+});
+
 module.exports = router;
